1e38cf0b35a84521a4f5ddc613715661b6a9be98
ViniMatu <vini.m.diogo@gmail.com>
2023-10-09 12:12:01 -0300

    Correção da Busca por ID
    Correção da busca por ID, estava usando paramêtros errados

--- src/repositories/jogosRepository.js
+++ src/repositories/jogosRepository.js
@@ -1,34 +1,37 @@
-const { readFile } = require('fs/promises')
-const file = './src/database/jogos.json'
+const { readFile } = require("fs/promises");
+const file = "./src/database/jogos.json";
 
-class JogoRepository{
-    constructor(){
-        this.file = file
-    }
+class JogoRepository {
+  constructor() {
+    this.file = file;
+  }
 
-    async __readFile() {
-        return JSON.parse(await readFile(this.file))
-    }
+  async __readFile() {
+    return JSON.parse(await readFile(this.file));
+  }
 
-    async getAllGames(){
-        const all = await this.__readFile()
-        if(all) 
-            return all
+  async getAllGames() {
+    const all = await this.__readFile();
+    if (all) return all;
 
-        return null
-    }
+    return null;
+  }
 
-    async getGame(id) {
-        const allGames = await this.__readFile()
-        return allGames.find(({id}) => id === id)
-    }
-
+  async getGame(idGame) {
+    const allGames = await this.__readFile();
+    return allGames.find((game) => {
+      if (parseInt(idGame) === game.app.appid) {
+        console.log(game);
+        return game;
+      }
+    });
+  }
 }
 
 const jogosRepository = new JogoRepository({
-    file: './src/database/jogos.json'
-})
+  file: "./src/database/jogos.json",
+});
 
 // jogosRepository.find(1).then(console.log).catch(err => console.log(err))
 
-module.exports = JogoRepository 
+module.exports = JogoRepository;

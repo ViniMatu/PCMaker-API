d59259b6112ca19e4fad9f7c9bdeb77bc2b7d293
ViniMatu <vini.m.diogo@gmail.com>
2023-11-08 10:47:50 -0300

    Update connectDatabase.js

--- src/database/connectDatabase.js
+++ src/database/connectDatabase.js
@@ -5,20 +5,15 @@ class DB{
   constructor() {}
 
   async connection(){
-    try{
-        const conn = await mysql.createConnection(process.env.DATABASE_URL)
-        console.log('Connected to PlanetScale!')
-        return conn
-    }catch(e){
-      console.log("Erro na conexão com o banco de dados: ", e)
-      throw e;
-    }
+      const conn = await mysql.createConnection(process.env.DATABASE_URL).catch(console.log("Conexão feita"))
+      console.log('Connected to PlanetScale!')
+      return conn
   } 
 
   async makeQuery(query){
       try{
         let conn = await this.connection()
-        const [rows, fields] = await conn.execute(query)
+        const [rows, fields] = await conn.execute(query).catch(console.log('Query feita'))
         conn.end()
         console.log("Conexão com PlanetScale fechada")
         return rows

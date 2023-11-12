bab2cd53502547df2587fd476dc226116bbdd1b1
ViniMatu <vini.m.diogo@gmail.com>
2023-11-12 01:16:25 -0300

    Implementação da lógica de recomendação
    Adicionado lógica de recomendação

---
+++ src/controller/recommendController.js
@@ -0,0 +1,19 @@
+const { request, response } = require("express")
+const RecommendService = require("../service/recommendService")
+const recommendService = new RecommendService()
+
+const getRecommend= async (request, response) => {
+    try{
+        const { ids } = request.params
+        const allIds = ids.split("&")
+        const recommendation = await recommendService.getRecommend(allIds)
+        return response.status(200).json(recommendation)
+    } catch (e){
+        console.log("Erro ao acessar o serviço de recomendação: ", e)
+        throw e;
+    }
+}
+
+module.exports = {
+    getRecommend
+}

8513fd02836ea6328b3609a84279d9aa6832d454
Lucas Kenzo Kuroki <lcskenzoo@gmail.com>
2023-11-19 01:15:59 -0300

    integração da api mercadoLivre(Amazon na versao 2.0 =)))
    No recommendedService foi adicionado uma função (searchComponents) que pega os modelos das peças e manda para a api do mercado livre(armazenada na pasta apiMarketPlace). Esta função é chamada dentro da getRecommend.

--- src/service/recommendService.js
+++ src/service/recommendService.js
@@ -1,9 +1,33 @@
 const JogoRepository = require("../repositories/jogosRepository")
+const scrapeMercadoLivre = require("../repositories/Marketplace");
 const jogoRepository = new JogoRepository()
 
 class RecommendService{
     constructor(){}
 
+    prepareSearchQuery(model) {
+        return model;
+    }
+
+    async searchComponents(recommendation){
+        try{
+            for(const category in recommendation){
+                if(recommendation.hasOwnProperty(category)){
+                    const piece = recommendation[category];
+                    //const searchQuery = `${piece.Model}`;
+                    const searchQuery = this.prepareSearchQuery(piece.Model);
+                    const searchResults = await scrapeMercadoLivre(searchQuery);
+                    if(searchResults.length > 0){
+                        piece.price = searchResults[0].price;
+                        piece.link = searchResults[0].link;
+                    }
+                }
+            }
+        }catch(e){
+            console.error("Erro na busca de componentes:", error);
+            throw e;
+        }
+    }
     async getRecommend(ids){
         try{
             let games = []
@@ -17,7 +41,9 @@ class RecommendService{
                 minimun: min,
                 recommended: rec
             }
-            
+            await this.searchComponents(recommendation.minimun);
+            await this.searchComponents(recommendation.recommended);
+
             return recommendation
         } catch (e){
             console.log("Erro ao acessar o repositório: ", e)

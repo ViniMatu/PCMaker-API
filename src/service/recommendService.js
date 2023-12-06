4cf68fadf582544ba2fd592a78aef9c8ce95c673
ViniMatu <vini.m.diogo@gmail.com>
2023-12-05 22:58:55 -0300

    Adição da lógica de Fonte

--- src/service/recommendService.js
+++ src/service/recommendService.js
@@ -81,6 +81,7 @@ class RecommendService{
             }
 
             await this.getMotherboard(recommendation, typeReq)
+            await this.getPowerSource(recommendation)
 
             return recommendation
         } catch(e){
@@ -134,7 +135,41 @@ class RecommendService{
             console.log("Erro ao escolher a placa-mãe: ", e)
             throw e
         }
+    }
 
+    async getPowerSource(recommend){
+        try{
+            let powerSource
+            const cpuModel = recommend['gpu']['Model']
+            if(cpuModel.includes("i3") || cpuModel.includes("Rayzen 3"))
+                powerSource = 450
+            else if(cpuModel.includes("i5") || cpuModel.includes("Rayzen 5"))
+                powerSource = 550
+            else 
+                powerSource = 650
+
+            const gpuModel = recommend[`cpu`][`Model`]
+            if(recommend[`cpu`][`Brand`] === "Intel"){
+                if(gpuModel.includes("RTX 40"))
+                    powerSource += 150
+                else
+                    powerSource += 100
+            } else if(recommend[`cpu`][`Brand`] === "AMD"){
+                if(gpuModel.includes("RX 7000"))
+                    powerSource += 200
+                else 
+                    powerSource += 100
+            }
+
+            let Powersuplie = {
+                Type: "fonte",
+                Powersuplie: powerSource
+            }
+
+            recommend.Powersuplie = Powersuplie
+        } catch (e){
+            console.log("Erro ao escolher Power Source")
+        }
     }
 }
 
